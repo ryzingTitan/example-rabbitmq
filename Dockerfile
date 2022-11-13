2dfcef6aa26c3828a9e3d153215e1352b18e3ae8
ryzingTitan <computerwhiz33@gmail.com>
2022-11-13 00:14:48 -0500

    Creating initial example RabbitMQ project

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM gradle:7.5.1-jdk17-alpine as build
+
+# build project within temporary Docker image
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle bootJar -info
+
+# build final Docker image using output from build image above
+FROM eclipse-temurin:17-jre-jammy
+EXPOSE 8080
+COPY --from=build /home/gradle/src/build/libs/example-rabbitmq-1.0.0.jar /app/example-rabbitmq-1.0.0.jar
+WORKDIR /app
+
+CMD ["java", "-jar", "example-rabbitmq-1.0.0.jar"]
---
+++ src/test/kotlin/com/example/rabbitmq/cucumber/common/LoggingStepDefs.kt
@@ -0,0 +1,59 @@
+package com.example.rabbitmq.cucumber.common
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import com.example.rabbitmq.cucumber.dtos.LogMessage
+import com.example.rabbitmq.domain.services.MessageReceiverService
+import com.example.rabbitmq.domain.services.MessageSenderService
+import io.cucumber.datatable.DataTable
+import io.cucumber.java.After
+import io.cucumber.java.Before
+import io.cucumber.java.DataTableType
+import io.cucumber.java.en.Then
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.slf4j.LoggerFactory
+
+class LoggingStepDefs {
+    @DataTableType
+    fun mapLogMessage(tableRow: Map<String, String>): LogMessage {
+        return LogMessage(
+            level = tableRow["level"].toString(),
+            message = tableRow["message"].toString()
+        )
+    }
+
+    @Before
+    fun setup() {
+        messageReceiverLogger = LoggerFactory.getLogger(MessageReceiverService::class.java) as Logger
+        messageSenderLogger = LoggerFactory.getLogger(MessageSenderService::class.java) as Logger
+
+        messageReceiverLogger.addAppender(appender)
+        messageSenderLogger.addAppender(appender)
+
+        appender.start()
+    }
+
+    @Then("the application will log the following messages:")
+    fun theApplicationWilLogTheFollowingMessages(table: DataTable) {
+        val expectedLogMessages: List<LogMessage> = table.tableConverter.toList(table, LogMessage::class.java)
+
+        val actualLogMessages = ArrayList<LogMessage>()
+
+        appender.list.forEach {
+            actualLogMessages.add(LogMessage(it.level.levelStr, it.message))
+        }
+
+        assertEquals(expectedLogMessages.sortedBy { it.message }, actualLogMessages.sortedBy { it.message })
+    }
+
+    @After
+    fun teardown() {
+        appender.stop()
+    }
+
+    private lateinit var messageReceiverLogger: Logger
+    private lateinit var messageSenderLogger: Logger
+
+    private val appender: ListAppender<ILoggingEvent> = ListAppender()
+}
